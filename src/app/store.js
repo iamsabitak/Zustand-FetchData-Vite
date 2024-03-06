3bbcb457a93204bfe2507dc3125e7a13473d7b54
sabita <sabitakhadka091@gmail.com>
2024-03-06 12:48:15 +0545

    Add initial state and data fetching function to useStore hook

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,16 @@
 import { create } from "zustand";
 
 export const useStore = create((set) => ({
-  
+  data: [],
+  fetchdata: async () => {
+    try {
+      const res = await fetch("https://dummyjson.com/users");
+      const data = await res.json();
+      const users = data.users;
+      console.log(users);
+      set({ data: users });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  },
 }));
