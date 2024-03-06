121228773a5b651e9fe5b5ae3eb0ca691c2fcfe5
sabita <sabitakhadka091@gmail.com>
2024-03-06 16:51:25 +0545

    Implement form component to post data to API

--- src/components/PostData.jsx
+++ src/components/PostData.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import { useStore } from "../app/store";
+
 function PostData() {
+  const [input, setInput] = useState("");
+  const { postdata } = useStore();
+
+  const handleSubmit = async () => {
+    const response = await postdata(input);
+    console.log(response);
+  };
   return (
-    <div>PostData</div>
-  )
+    <div>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
 
-export default PostData
+export default PostData;
