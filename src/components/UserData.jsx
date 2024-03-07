f5643f65909a0ec8b2e346195d69ec171e0bf23c
sabita <sabitakhadka091@gmail.com>
2024-03-07 11:44:33 +0545

    disable eslint

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -6,6 +6,7 @@ function UserData() {
  const { fetchdata, data } = useStore();
  useEffect(() => {
    fetchdata();
+ // eslint-disable-next-line react-hooks/exhaustive-deps
  }, []);
  return (
    <div>
