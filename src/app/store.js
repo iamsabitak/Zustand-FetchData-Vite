654073b71920975e46bb5bb52ae13fec8bf2e7e6
sabita <sabitakhadka091@gmail.com>
2024-03-07 14:03:32 +0545

    added persist to save the data in localstorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,29 +1,38 @@
 import axios from "axios";
-import { create } from "zustand";
 
-export const useStore = create((set) => ({
-  data: [],
-  fetchdata: async () => {
-    try {
-      const res = await fetch("https://dummyjson.com/users");
-      const data = await res.json();
-      const users = data.users;
-      console.log(users);
-      set({ data: users });
-    } catch (error) {
-      console.error("Error fetching data:", error);
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export const useStore = create(
+  persist(
+    (set) => ({
+      data: [],
+      fetchdata: async () => {
+        try {
+          const res = await fetch("https://dummyjson.com/users");
+          const data = await res.json();
+          const users = data.users;
+          console.log(users);
+          set({ data: users });
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      },
+      postdata: async (input) => {
+        try {
+          const res = await axios.post(
+            "https://jsonplaceholder.typicode.com/users",
+            { name: input }
+          );
+          // set({ data: [res.data] });
+          return res.data;
+        } catch (error) {
+          console.log("Error", error);
+        }
+      },
+    }),
+    {
+      name: "data-storage",
     }
-  },
-  postdata: async (input) => {
-    try {
-      const res = await axios.post(
-        "https://jsonplaceholder.typicode.com/users",
-        { name: input }
-      );
-      // set({ data: [res.data] });
-      return res.data;
-    } catch (error) {
-      console.log("Error", error);
-    }
-  },
-}));
+  )
+);
