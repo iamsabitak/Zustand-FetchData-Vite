8d5e2094f036b34bb9e934ebfbb6351099388239
sabita <sabitakhadka091@gmail.com>
2024-03-07 14:02:16 +0545

    added userfisrtName lastname and age in a list to show in ui

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import { useStore } from "../app/store";
 
-
 function UserData() {
- const { fetchdata, data } = useStore();
- useEffect(() => {
-   fetchdata();
- // eslint-disable-next-line react-hooks/exhaustive-deps
- }, []);
- return (
-   <div>
-     {data.map((user) => (
-       <li key={user.id}>{user.firstName}</li>
-     ))}
-   </div>
- );
+  const { fetchdata, data } = useStore();
+  useEffect(() => {
+    fetchdata();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div>
+      {data.map((user) => (
+        <>
+          <li key={user.id}>
+            {user.firstName} {user.lastName}
+          </li>
+          <li>{user.age}</li>
+        </>
+      ))}
+    </div>
+  );
 }
-
 
 export default UserData;
